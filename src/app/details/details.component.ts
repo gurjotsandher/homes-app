efd3120dd29872e95768c939c07e11f4a7406b20
Gurjot Sandher <gsa143@sfu.ca>
2024-04-29 20:54:26 -0700

    testing protected action

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   imports: [CommonModule],
   template: `
     <p>
-      details works!
+      From the protected branch.
     </p>
   `,
   styleUrls: ['./details.component.css']
