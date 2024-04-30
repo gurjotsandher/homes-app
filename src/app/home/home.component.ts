22813efb284f55671752de2f37816ebf9954d863
Gurjot Sandher <gsa143@sfu.ca>
2024-04-29 20:04:21 -0700

    updated src for images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent {
       name: 'Acme Fresh Start Housing',
       city: 'Chicago',
       state: 'IL',
-      photo: '/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      photo: './assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
       availableUnits: 4,
       wifi: true,
       laundry: true
@@ -37,7 +37,7 @@ export class HomeComponent {
       name: 'A113 Transitional Housing',
       city: 'Santa Monica',
       state: 'CA',
-      photo: '/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg',
+      photo: './assets/brandon-griggs-wR11KBaB86U-unsplash.jpg',
       availableUnits: 0,
       wifi: false,
       laundry: true
@@ -47,7 +47,7 @@ export class HomeComponent {
       name: 'Warm Beds Housing Support',
       city: 'Juneau',
       state: 'AK',
-      photo: '/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
+      photo: './assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
       availableUnits: 1,
       wifi: false,
       laundry: false
@@ -57,7 +57,7 @@ export class HomeComponent {
       name: 'Homesteady Housing',
       city: 'Chicago',
       state: 'IL',
-      photo: '/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg',
+      photo: './assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg',
       availableUnits: 1,
       wifi: true,
       laundry: false
@@ -67,7 +67,7 @@ export class HomeComponent {
       name: 'Happy Homes Group',
       city: 'Gary',
       state: 'IN',
-      photo: '/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
+      photo: './assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
       availableUnits: 1,
       wifi: true,
       laundry: false
@@ -77,7 +77,7 @@ export class HomeComponent {
       name: 'Hopeful Apartment Group',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
+      photo: './assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
       availableUnits: 2,
       wifi: true,
       laundry: true
@@ -87,7 +87,7 @@ export class HomeComponent {
       name: 'Seriously Safe Towns',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
+      photo: './assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
       availableUnits: 5,
       wifi: true,
       laundry: true
@@ -97,7 +97,7 @@ export class HomeComponent {
       name: 'Hopeful Housing Solutions',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/r-architecture-GGupkreKwxA-unsplash.jpg',
+      photo: './assets/r-architecture-GGupkreKwxA-unsplash.jpg',
       availableUnits: 2,
       wifi: true,
       laundry: true
@@ -107,7 +107,7 @@ export class HomeComponent {
       name: 'Seriously Safe Towns',
       city: 'Oakland',
       state: 'CA',
-      photo: '/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg',
+      photo: './assets/saru-robert-9rP3mxf8qWI-unsplash.jpg',
       availableUnits: 10,
       wifi: false,
       laundry: false
@@ -117,7 +117,7 @@ export class HomeComponent {
       name: 'Capital Safe Towns',
       city: 'Portland',
       state: 'OR',
-      photo: '/assets/webaliser-_TPTXZd9mOo-unsplash.jpg',
+      photo: './assets/webaliser-_TPTXZd9mOo-unsplash.jpg',
       availableUnits: 6,
       wifi: true,
       laundry: true
